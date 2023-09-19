fc60f09ae0bcd5e7a2f61fe961c13c98e695d231
demonic29 <khaleesikomodo@gmail.com>
2023-09-19 15:31:54 +0900

    update

---
+++ js/index.js
@@ -0,0 +1,32 @@
+let btn = function(){
+    document.getElementById('demo').innerHTML = Date();
+}
+
+document.getElementById("js").innerHTML = "Hello JavaScript";
+
+let on = function(){
+    document.getElementById("image").src = "https://img.freepik.com/free-vector/light-bulb_78370-520.jpg?w=826&t=st=1695100332~exp=1695100932~hmac=53e4d653cb2d8f56f6827391f761c8a8d71affa40523236339be87e6a135db3e";
+}
+
+let off = function(){
+    document.getElementById("image").src = "https://img.freepik.com/free-vector/doodle-light-bulb_1034-530.jpg?w=826&t=st=1695100700~exp=1695101300~hmac=f01bbef59744f1a26825ce78a468e107cfbe2daa259926695f18da2baa285e7e";
+}
+
+// window.alert("You are GAY");
+
+let print = function(){
+    window.print();
+}   
+
+let a,b,c;
+
+a = "Put"
+b = "your hands on"
+c = "My shoulder"
+
+let lyrics = function(){
+    document.getElementById("a").innerHTML = a;
+    document.getElementById("b").innerHTML = b;
+    document.getElementById("c").innerHTML = c;
+}
+
